6611b0ad2a5fe9d0ad2c58c3c12534a0bc4bc740
Nelaty <m.gensheimer@yahoo.de>
2017-12-11 17:24:24 +0100

    Timer, shooting, collision

---
+++ Assets/Scripts/BasicProjectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicProjectile : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float coolingStrength;
+    [SerializeField] private float projectileOffset;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position += projectileOffset * transform.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += Time.deltaTime * movementSpeed * transform.right;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        /*
+         
+        HitzeBlockClass hit = collision.GetComponent<HitzeBlockClass>()
+        
+        if(hit != null)
+        {
+            hit->AddCooling(coolingStrength);
+        }
+
+        */
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    private float currentTime;
+    [SerializeField] private int pointsPerSecondReduce;
+    [SerializeField] private int maxPoints;
+
+    // Use this for initialization
+    void Awake()
+    {
+        currentTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+    }
+
+    public string GetTime()
+    {
+        int minutes = (int)(currentTime / 60);
+        int seconds = (int)(currentTime - minutes * 60);
+
+        string minuteString = minutes.ToString();
+        string secondString = seconds.ToString();
+
+        if (minutes < 10)
+            minuteString = "0" + minuteString;
+        if (seconds < 10)
+            secondString = "0" + secondString;
+
+        return minuteString + ":" + secondString;
+    }
+
+    public int GetTimeScore()
+    {
+        return Mathf.Clamp(maxPoints - (int)(currentTime * pointsPerSecondReduce), 0, maxPoints);
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width - 150, 50, 100, 50), "Time: " + GetTime() + "\nDEBUG: " + GetTimeScore().ToString());
+    }
+}
